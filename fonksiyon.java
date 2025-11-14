b4e4db668e2a66e06641fba4b038b7ae952afde3
alperentez <tezgidenalperen@gmail.com>
2025-11-14 19:32:40 +0300

    fonksiyon.java

---
+++ fonksiyon.java
@@ -0,0 +1,12 @@
+String hava() {
+    return "Hava iyi";
+}
+
+int toplama(int a, int b){
+    return a+b;
+}
+
+void main() {
+    System.out.println(hava());
+    System.out.println("2 ile 2nin toplamı:"+toplama(2,2));
+}
