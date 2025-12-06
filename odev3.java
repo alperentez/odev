1395fef5a9dd01dfb77dfb906c0c2f557861b00d
alperentez <tezgidenalperen@gmail.com>
2025-12-06 14:32:50 +0300

    odev3 string

---
+++ odev3.java
@@ -0,0 +1,28 @@
+class Main {
+    public static void main(String[] args) {
+    String plaka1="34";
+    String plaka2="35";
+    String plaka3="52";
+    String plaka4="59";
+        if(plaka1.equals("34")) {
+            System.out.println("bu plaka istanbul plakası");
+        }else {
+            System.out.println("bu plaka istanbul plakası değil");
+        }
+        if(plaka2.equals("34")) {
+            System.out.println("bu plaka istanbul plakası");
+        }else {
+            System.out.println("bu plaka istanbul plakası değil");
+        }
+        if(plaka3.equals("34")) {
+            System.out.println("bu plaka istanbul plakası");
+        }else {
+            System.out.println("bu plaka istanbul plakası değil");
+        }
+        if(plaka4.equals("34")) {
+            System.out.println("bu plaka istanbul plakası");
+        }else {
+            System.out.println("bu plaka istanbul plakası değil");
+        }
+    }
+}
