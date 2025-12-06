4a1c10160aa9cee7ee2a937163a902bdb2a0ffa8
alperentez <tezgidenalperen@gmail.com>
2025-12-06 14:44:17 +0300

    odev4.java

---
+++ odev4.java
@@ -0,0 +1,10 @@
+void main(String[] args) {
+    double sinav1=75;
+    double sinav2=80;
+    double ortalama=(sinav1*0.4)+(sinav2*0.6);
+            if (ortalama>=75){
+                System.out.println("sınıfı geçtiniz");
+            }else if (ortalama<75){
+                System.out.println("sınıfta kaldın");
+            }
+}
